2609ecec8d2115f43a273684f27cc8ad7b3724b2
Pavel Osokin <pachkaosokin@yandex.ru>
2024-06-18 22:06:59 +0500

    Реализован внешний вид главного окна. Осокин

---
+++ Classes/Connection.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ПР43_Осокин.Classes
+{
+    public class Connection
+    {
+        public static readonly string connection = "server=localhost;uid=root;database=pr43;";
+        public static readonly MySqlServerVersion version = new MySqlServerVersion(new Version(8, 0, 11));
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -20,9 +20,12 @@ namespace ПР43_Осокин
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow init;
         public MainWindow()
         {
             InitializeComponent();
+            init = this;
+            frame.Navigate(new View.Main());
         }
     }
 }
