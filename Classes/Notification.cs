46f227394f75b3b92778da9fe09f35a349dfdf6e
Pavel Osokin <pachkaosokin@yandex.ru>
2024-09-19 19:11:11 +0500

    Создание классов Notification и RealyCommand. Осокин

---
+++ Classes/Notification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ПР43_Осокин.Classes
+{
+    public class Notification : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
